85335345922defdbb84b749c8f3f31a7d23338b3
foonia <ljh9032a@naver.com>
2020-08-13 16:49:34 +0900

    Set up your distribution environment

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
