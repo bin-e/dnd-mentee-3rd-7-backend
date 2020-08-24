64542874d845c95005deadbef354e323bc59cd27
foonia <ljh9032a@naver.com>
2020-08-24 17:23:04 +0900

    feature: add post model

--- backend/greene_api/models.py
+++ backend/greene_api/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 class User(AbstractUser):
-    pass
+    class Meta:
+        db_table = "user"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    like = models.IntegerField(default=0)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        )
+
+    def __str__(self):
+        return f"{user}, {title}"
