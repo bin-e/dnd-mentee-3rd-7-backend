59943f964a2c7adc9fbd44a57b78cc854b05f959
foonia <ljh9032a@naver.com>
2020-08-27 00:47:54 +0900

    feature: add model in admin page

--- backend/greene_api/admin.py
+++ backend/greene_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Post
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', )
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'like', 'date_created', 'user')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
