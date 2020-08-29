01bdd059a81c3a07ba173ddf7a37e482bb1236cc
foonia <ljh9032a@naver.com>
2020-08-29 22:04:07 +0900

    Fix: made pemission in Post view

--- backend/greene_api/views.py
+++ backend/greene_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .models import User, Post
@@ -22,14 +22,12 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    authentication_classes = (JSONWebTokenAuthentication, )
+    authentication_classes = (JSONWebTokenAuthentication,)
 
     def get_permissions(self):
-        if self.action == 'create':
+        if self.action in ('create',):
              permission_classes = (AllowAny,)
-        elif self.action == 'list':
-            permission_classes = (IsAdminUser,)
-        elif self.action in ('update', 'destroy'):
+        elif self.action in ('update', 'partial_update', 'destroy',):
             permission_classes = (IsAuthenticated,)
         else:
             permission_classes = (IsAdminUser,)
@@ -47,3 +45,13 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = (JSONWebTokenAuthentication,)
+    
+    def get_permissions(self):
+        if self.action in ('list', 'retrieve',):
+             permission_classes = (AllowAny,)
+        elif self.action in ('create', 'partial_update', 'destroy',):
+            permission_classes = (IsAuthenticated,)
+        else:
+            permission_classes = (IsAdminUser,)  
+        return [permission() for permission in permission_classes]
