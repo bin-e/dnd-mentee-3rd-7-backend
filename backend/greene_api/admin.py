7db059ed1abd057286c4e321f80094507a1d5a49
foonia <ljh9032a@naver.com>
2020-09-03 12:29:05 +0900

    Feature: made a commet factory for generating datas

--- backend/greene_api/admin.py
+++ backend/greene_api/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import User, Post, Hashtag
+from .models import User, Post, Comment, Hashtag
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'username', 'email', 'is_staff', 'is_superuser',)
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'like', 'date_created', 'user',)
+    list_display = ('id', 'title', 'like', 'date_created', 'user',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'content',)
 
 class HashtagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
@@ -14,3 +17,4 @@ class HashtagAdmin(admin.ModelAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Hashtag, HashtagAdmin)
+admin.site.register(Comment, CommentAdmin)
